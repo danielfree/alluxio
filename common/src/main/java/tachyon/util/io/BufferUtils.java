4df44a811f2147549a477eb534938de541cf5f22
Daniel Yu <danielmethod@gmail.com>
2016-04-15 16:33:13 +0800

    make bufferutils thread safe

--- common/src/main/java/tachyon/util/io/BufferUtils.java
+++ common/src/main/java/tachyon/util/io/BufferUtils.java
@@ -51,13 +51,13 @@ public final class BufferUtils {
    * <p>
    * NOTE: DirectByteBuffers are not guaranteed to be garbage-collected immediately after their
    * references are released and may lead to OutOfMemoryError. This function helps by calling the
-   * Cleaner method of a DirectByteBuffer explicitly. See <a
-   * href="http://stackoverflow.com/questions/1854398/how-to-garbage-collect-a-direct-buffer-java"
-   * >more discussion</a>.
+   * Cleaner method of a DirectByteBuffer explicitly. See <a href=
+   * "http://stackoverflow.com/questions/1854398/how-to-garbage-collect-a-direct-buffer-java" >more
+   * discussion</a>.
    *
    * @param buffer the byte buffer to be unmapped, this must be a direct buffer
    */
-  public static void cleanDirectBuffer(ByteBuffer buffer) {
+  public static synchronized void cleanDirectBuffer(ByteBuffer buffer) {
     Preconditions.checkNotNull(buffer);
     Preconditions.checkArgument(buffer.isDirect(), "buffer isn't a DirectByteBuffer");
     try {
@@ -67,7 +67,10 @@ public final class BufferUtils {
       }
       final Object cleaner = sByteBufferCleanerMethod.invoke(buffer);
       if (cleaner == null) {
-        LOG.error("Failed to get cleaner for ByteBuffer");
+        if (buffer.capacity() > 0) {
+          LOG.warn("Failed to get cleaner for ByteBuffer: {}", buffer.getClass().getName());
+        }
+        // The cleaner could be null when the buffer is initialized as zero capacity.
         return;
       }
       if (sCleanerCleanMethod == null) {
@@ -75,8 +78,9 @@ public final class BufferUtils {
       }
       sCleanerCleanMethod.invoke(cleaner);
     } catch (Exception e) {
-      LOG.warn("Fail to unmap direct buffer due to " + e.getMessage(), e);
+      LOG.warn("Failed to unmap direct ByteBuffer: {}", buffer.getClass().getName(), e);
     } finally {
+      // Force to drop reference to the buffer to clean
       buffer = null;
     }
   }
